b06065ca13a53037ee73daa57f8424d73b1001c9
Binary-Quest <163310128+Binary-Quest@users.noreply.github.com>
2025-04-18 16:52:46 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -11,7 +11,7 @@ from Script import script
 from plugins.dbusers import db
 from pyrogram import Client, filters, enums
 from plugins.users_api import get_user, update_user_info
-from pyrogram.errors import ChatAdminRequired, FloodWait
+from pyrogram.errors import *
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from utils import AUTH_CHANNEL, verify_user, check_token, check_verification, get_token
 from config import *
